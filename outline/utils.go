d53eda9e599274b2bc3a9dc34db95308be8f97b1
nepriyatelev <nepriyatelev@icloud.com>
2025-09-24 23:26:41 +0300

    Add formatDuration function to format time.Duration as string

--- outline/utils.go
+++ outline/utils.go
@@ -1,13 +1,44 @@
 package outline
 
 import (
-    "strings"
+	"fmt"
+	"strings"
+	"time"
 )
 
 func maskSecretPath(raw, secret string) string {
-    if secret == "" {
+	if secret == "" {
 		return raw
 	}
 
 	return strings.ReplaceAll(raw, "/"+secret+"/", "/*****/")
 }
+
+func formatDuration(d time.Duration) string {
+    // Определяем знак и работаем с абсолютным значением
+    sign := ""
+    if d < 0 {
+        sign = "-"
+        d = -d
+    }
+
+    // Часы
+    h := int64(d.Hours())
+    if h != 0 {
+        return fmt.Sprintf("%s%dh", sign, h)
+    }
+    // Минуты
+    m := int64(d.Minutes())
+    if m != 0 {
+        return fmt.Sprintf("%s%dm", sign, m)
+    }
+    // Секунды
+    s := int64(d.Seconds())
+    if s != 0 {
+        return fmt.Sprintf("%s%ds", sign, s)
+    }
+    // Если всё равно ноль, игнорируем знак и возвращаем "0s"
+    return "0s"
+}
+
+
