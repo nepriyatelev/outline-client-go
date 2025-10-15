8a8036849229e71b37c2ed17b2a6522f0bf66f18
nepriyatelev <nepriyatelev@icloud.com>
2025-10-15 23:49:02 +0300

    Refactor formatDuration function for improved readability and maintainability; add setIDInPath function for URL manipulation

--- outline/utils.go
+++ outline/utils.go
@@ -2,6 +2,7 @@ package outline
 
 import (
 	"fmt"
+	"net/url"
 	"strings"
 	"time"
 )
@@ -15,30 +16,33 @@ func maskSecretPath(raw, secret string) string {
 }
 
 func formatDuration(d time.Duration) string {
-    // Определяем знак и работаем с абсолютным значением
-    sign := ""
-    if d < 0 {
-        sign = "-"
-        d = -d
-    }
+	// Определяем знак и работаем с абсолютным значением
+	sign := ""
+	if d < 0 {
+		sign = "-"
+		d = -d
+	}
 
-    // Часы
-    h := int64(d.Hours())
-    if h != 0 {
-        return fmt.Sprintf("%s%dh", sign, h)
-    }
-    // Минуты
-    m := int64(d.Minutes())
-    if m != 0 {
-        return fmt.Sprintf("%s%dm", sign, m)
-    }
-    // Секунды
-    s := int64(d.Seconds())
-    if s != 0 {
-        return fmt.Sprintf("%s%ds", sign, s)
-    }
-    // Если всё равно ноль, игнорируем знак и возвращаем "0s"
-    return "0s"
+	// Часы
+	h := int64(d.Hours())
+	if h != 0 {
+		return fmt.Sprintf("%s%dh", sign, h)
+	}
+	// Минуты
+	m := int64(d.Minutes())
+	if m != 0 {
+		return fmt.Sprintf("%s%dm", sign, m)
+	}
+	// Секунды
+	s := int64(d.Seconds())
+	if s != 0 {
+		return fmt.Sprintf("%s%ds", sign, s)
+	}
+	// Если всё равно ноль, игнорируем знак и возвращаем "0s"
+	return "0s"
 }
 
-
+func setIDInPath(basePath *url.URL, id uint64) {
+    replacedPath := strings.Replace(basePath.Path, "{id}", fmt.Sprint(id), 1)
+    basePath.Path = replacedPath
+}
