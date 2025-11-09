1a8678427d3b4e02afd34d82c4b61bfd0cfe7bc8
nepriyatelev <nepriyatelev@icloud.com>
2025-11-09 21:28:37 +0300

    feat(errors): add ParseURLError and UnmarshalError types

--- outline/errors.go
+++ outline/errors.go
@@ -4,6 +4,15 @@ import (
 	"fmt"
 )
 
+type ParseURLError struct {
+	BaseURL string
+	Err     error
+}
+
+func (e *ParseURLError) Error() string {
+	return fmt.Sprintf("outline client error: invalid baseURL %q: %v", e.BaseURL, e.Err)
+}
+
 // ClientError is the root error type for the Outline client.
 type ClientError struct {
 	Code    int
@@ -16,10 +25,28 @@ func (e *ClientError) Error() string {
 
 // Predefined errors for specific statuses.
 var (
-	errUnexpected = func(statusCode int) *ClientError {
+	errUnexpected = func(statusCode int, body []byte) *ClientError {
 		return &ClientError{
 			Code:    statusCode,
-			Message: "An unexpected error occurred.",
+			Message: fmt.Sprintf("An unexpected error occurred: body=%s", string(body)),
+		}
+	}
+	
+	errParseBaseURL = func(baseURL string, err error) *ParseURLError {
+		return &ParseURLError{
+			BaseURL: baseURL,
+			Err:     err,
 		}
 	}
 )
+
+// UnmarshalError содержит детали ошибки при распаковке JSON
+type UnmarshalError struct {
+	Data []byte
+	Type string
+	Err  error
+}
+
+func (e *UnmarshalError) Error() string {
+	return fmt.Sprintf("unmarshal %s failed: %v", e.Type, e.Err)
+}
