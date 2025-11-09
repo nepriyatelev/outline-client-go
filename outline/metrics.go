f8f4dbdcd0b5c9e0ba430f7df35304cf6a0fb5f2
nepriyatelev <nepriyatelev@icloud.com>
2025-11-09 21:29:19 +0300

    refactor(metrics): simplify GetMetricsTransfer function

--- outline/metrics.go
+++ outline/metrics.go
@@ -2,12 +2,13 @@ package outline
 
 import (
 	"context"
-	"encoding/json"
 	"net/http"
 
 	"github.com/nepriyatelev/outline-client-go/internal/contracts"
 	"github.com/nepriyatelev/outline-client-go/outline/types"
 )
+
+// === Transfer Metrics ===
 
 func (c *Client) GetMetricsTransfer(ctx context.Context) (*types.MetricsTransfer, error) {
 	req := &contracts.Request{
@@ -17,15 +18,12 @@ func (c *Client) GetMetricsTransfer(ctx context.Context) (*types.MetricsTransfer
 		Body:    nil,
 	}
 
+	c.logRequest(ctx, "GetMetricsTransfer", req)
+
 	resp, err := c.doer.Do(ctx, req)
 	if err != nil {
 		return nil, err
 	}
 
-	var metricsTransfer *types.MetricsTransfer
-	if err = json.Unmarshal(resp.Body, metricsTransfer); err != nil {
-		return nil, err
-	}
-
-	return metricsTransfer, nil
+	return unmarshalJSONWithError[types.MetricsTransfer](resp.Body)
 }
