f2a54a089d65f34bfba2955c2107b1c970146517
nepriyatelev <nepriyatelev@icloud.com>
2026-01-24 00:48:19 +0300

    feat(duration): add formatDuration function and corresponding tests

---
+++ outline/duration.go
@@ -0,0 +1,33 @@
+package outline
+
+import (
+	"fmt"
+	"time"
+)
+
+func formatDuration(d time.Duration) string {
+	// Определяем знак и работаем с абсолютным значением
+	sign := ""
+	if d < 0 {
+		sign = "-"
+		d = -d
+	}
+
+	// Часы
+	h := int64(d.Hours())
+	if h != 0 {
+		return fmt.Sprintf("%s%dh", sign, h)
+	}
+	// Минуты
+	m := int64(d.Minutes())
+	if m != 0 {
+		return fmt.Sprintf("%s%dm", sign, m)
+	}
+	// Секунды
+	s := int64(d.Seconds())
+	if s != 0 {
+		return fmt.Sprintf("%s%ds", sign, s)
+	}
+	// Если всё равно ноль, игнорируем знак и возвращаем "0s"
+	return "0s"
+}
