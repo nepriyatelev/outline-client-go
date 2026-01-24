2818cd3750236019fc497e956214ab3a047a41f8
nepriyatelev <nepriyatelev@icloud.com>
2026-01-25 01:59:59 +0300

    docs(duration): translate comments to English for formatDuration

--- outline/duration.go
+++ outline/duration.go
@@ -6,28 +6,28 @@ import (
 )
 
 func formatDuration(d time.Duration) string {
-	// Определяем знак и работаем с абсолютным значением
+	// Determine the sign and work with the absolute value
 	sign := ""
 	if d < 0 {
 		sign = "-"
 		d = -d
 	}
 
-	// Часы
+	// Hours
 	h := int64(d.Hours())
 	if h != 0 {
 		return fmt.Sprintf("%s%dh", sign, h)
 	}
-	// Минуты
+	// Minutes
 	m := int64(d.Minutes())
 	if m != 0 {
 		return fmt.Sprintf("%s%dm", sign, m)
 	}
-	// Секунды
+	// Seconds
 	s := int64(d.Seconds())
 	if s != 0 {
 		return fmt.Sprintf("%s%ds", sign, s)
 	}
-	// Если всё равно ноль, игнорируем знак и возвращаем "0s"
+	// If still zero, ignore the sign and return "0s"
 	return "0s"
 }
