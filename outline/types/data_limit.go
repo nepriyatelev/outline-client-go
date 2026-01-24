1a1ecc60d11d345aaf120d58aaddf75110386219
nepriyatelev <nepriyatelev@icloud.com>
2026-01-25 01:57:53 +0300

    docs(types): clarify Limit struct and Bytes field documentation

--- outline/types/data_limit.go
+++ outline/types/data_limit.go
@@ -1,5 +1,7 @@
 package types
 
+// Limit represents a data transfer limit for an access key.
+// The zero value indicates no limit.
 type Limit struct {
-	Bytes uint64 `json:"bytes"`
+	Bytes uint64 `json:"bytes"` // Bytes is the maximum number of bytes allowed for data transfer. A value of 0 means no limit is enforced.
 }
